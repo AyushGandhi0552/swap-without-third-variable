c1295badf9a0f81da71677a4fec19c4960ef3576
ayush <ayush@AYUSHGANDHI>
2024-01-29 08:07:04 +0530

    Add project files.

---
+++ swap without/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap_without
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            a = a * b; 
+            b = a / b; 
+            a = a / b; 
+            Console.WriteLine(a); 
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
